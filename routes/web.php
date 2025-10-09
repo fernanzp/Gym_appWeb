f2d2945c6ed39978059523c8cece42f13ccb5bf3
Fernando <fperez23@ucol.mx>
2025-10-08 18:19:10 -0600

    Vista del login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Ruta base
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Ruta para el dashboard
+Route::view('/dashboard', 'dashboard');
+
+//Ruta para el login
+Route::view('/login', 'login');
